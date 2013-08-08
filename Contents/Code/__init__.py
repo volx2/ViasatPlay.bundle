c28978a48f1477abcbd083a2bd78cdf3e4cfefb2
JeppeTh <jesper.thorell@home.se>
2013-08-08 23:00:08 +0200

    Update __init__.py
    * added clips
    * added 'Search Program'

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,3 +1,4 @@
+import datetime
 TITLE  = 'Viasat Play'
 PREFIX = '/video/viasatplay'
 
@@ -34,7 +35,7 @@ CHANNELS = [
         'url':      'http://www.viasat4play.no',
         'thumb':    R('viasat4_norway.png'),
         'desc':     unicode('Viasat 4 er en norsk underholdnings- og sportskanal fra Modern Times Group (MTG). Kanalen startet sendinger 8. september 2007 i forbindelse med utbyggingen av det digitale bakkenettet.')
-    },	
+    },    
     {
         'title':    'TV3 Play Danmark',
         'url':      'http://www.tv3play.dk',
@@ -69,8 +70,37 @@ def MainMenu():
                 summary = channel['desc']
             )
         )
+
+    oc.add(InputDirectoryObject(key    = Callback(Search),
+                                title  = 'Search Program',
+                                prompt = 'Search Program'
+                                )
+           )
     
     return oc
+
+####################################################################################################
+def Search (query):
+
+    query = unicode(query)
+    oc = ObjectContainer(title1=TITLE, title2='Search Results')
+
+    for channel in CHANNELS:
+        for video in AllPrograms(channel['title'], channel['url']).objects:
+            if len(query) == 1 and query.lower() == video.title[0].lower():
+                # In case of single character - only compare initial character.
+                oc.add(video)
+            elif len(query) > 1 and query.lower() in video.title.lower():
+                oc.add(video)
+
+    if len(oc) == 0:
+        return MessageContainer(
+            "Search Results",
+            "Did not find any result for '%s'" % query
+            )
+    else:
+        oc.objects.sort(key=lambda obj: obj.title)
+        return oc
 
 ####################################################################################################
 @route(PREFIX + '/ChannelMenu')
@@ -88,6 +118,21 @@ def ChannelMenu(title, base_url, thumb):
                     id = 'latest_programs'
                 ), 
             title = "Latest programs", 
+            thumb = thumb
+        )
+    ) 
+
+    oc.add(
+        DirectoryObject(
+            key = 
+                Callback(
+                    Clips, 
+                    base_url = base_url, 
+                    videos_url = base_url + "/mobileapi/featured",
+                    title = "Latest clips",
+                    id    = 'latest_clips'
+                ), 
+            title = "Latest clips", 
             thumb = thumb
         )
     ) 
@@ -129,19 +174,23 @@ def AllPrograms(title, base_url):
     programsInfo = JSON.ObjectFromURL(base_url + "/mobileapi/format")
     for section in programsInfo['sections']:
         for program in section['formats']:
+            # Samsung causes troubles with empty Descriptions...
+            mySummary = None
+            if program['description'] != "":
+                mySummary = unicode(program['description'])
             oc.add(
                 DirectoryObject(
                     key = 
                         Callback(
                             Seasons,
-                            title = unicode(program['title']),
-                            summary = unicode(program['description']),
+                            title    = unicode(program['title']),
+                            summary  = mySummary,
                             base_url = base_url,
-                            id = program['id']
+                            id       = program['id']
                         ),
-                    title = unicode(program['title']),
-                    summary = unicode(program['description']),
-                    thumb = GetImgUrl(program['image'])
+                    title   = unicode(program['title']),
+                    summary = mySummary,
+                    thumb   = GetImgUrl(program['image'])
                 )
              )
   
@@ -152,52 +201,78 @@ def AllPrograms(title, base_url):
 def Seasons(title, summary, base_url, id):
     if summary:
         summary = unicode(summary)
-        
+
     oc = ObjectContainer(title2 = unicode(title))
   
     seasonsInfo = JSON.ObjectFromURL(base_url + "/mobileapi/detailed?formatid=" + id)
     for season in seasonsInfo['formatcategories']:
-        oc.add(
-            DirectoryObject(
-                key = 
-                    Callback(
-                        Episodes, 
-                        title = unicode(season['name']),
-                        base_url = base_url, 
-                        videos_url = season['videos_call'],
-                        id = 'video_program',
-                        art = GetImgUrl(seasonsInfo['format']['image'])
-                    ), 
-                title = unicode(season['name']), 
-                summary = summary, 
-                thumb = GetImgUrl(season['image']),
-                art = GetImgUrl(seasonsInfo['format']['image'])
-            )
-        )     
+        seasonName   = unicode(season['name'])
+        seasonImgUrl = GetImgUrl(seasonsInfo['format']['image'])
+        videos_url   = season['videos_call']
+        if len(seasonsInfo['formatcategories']) > 1:
+            oc.add(
+                DirectoryObject(
+                    key = 
+                    Callback(Episodes, 
+                             title      = seasonName,
+                             base_url   = base_url, 
+                             videos_url = videos_url,
+                             id         = 'video_program',
+                             art        = seasonImgUrl
+                        ), 
+                    title   = seasonName, 
+                    summary = summary, 
+                    thumb   = GetImgUrl(season['image']),
+                    art     = seasonImgUrl
+                    )
+                )
+        else:
+            return Episodes(seasonName,
+                            base_url,
+                            videos_url,
+                            'video_program',
+                            seasonImgUrl)
 
     return oc
  
 ####################################################################################################
 @route(PREFIX + '/Episodes')
 def Episodes(title, base_url, videos_url, id = None, art = None):
-    oc = ObjectContainer(title2 = unicode(title))
-    
+    oc        = ObjectContainer(title2 = unicode(title))
+    episodeOc = ObjectContainer(title2 = unicode(title))
+
     try:
         videosInfo = JSON.ObjectFromURL(videos_url)
     except:
-        videosInfo = None
-        
-    if videosInfo:
-        if id:
-            videos = videosInfo[id]
-        else:
-            videos = videosInfo
-            
+        oc.header  = "Sorry"
+        oc.message = "No programs found."
+        return oc
+
+    if id:
+        videos = videosInfo[id]
+    else:
+        videos = videosInfo
+
+    if id == 'video_program' and videosInfo['video_clip'] != None:
+        oc.add(DirectoryObject(
+                key = Callback(Clips,
+                               base_url   = base_url,
+                               videos_url = videos_url,
+                               title      = title,
+                               id         = 'video_clip',
+                               art        = art
+                               ), 
+                title = "Klipp", 
+                thumb = R(THUMB), 
+                art   = R(ART)
+                )
+               )
+    if videos != None:
         for video in videos:
-            oc.add(
+            episodeOc.add(
                 EpisodeObject(
                     url = base_url + '/play/' + video['id'],
-                    title = unicode(video['title']),
+                    title = unicode(video['title'] + " - " + video['summary']),
                     summary = unicode(video['description']),
                     show = unicode(video['formattitle']),
                     art = art,
@@ -206,16 +281,53 @@ def Episodes(title, base_url, videos_url, id = None, art = None):
                     duration = int(video['length']) * 1000,
                     season = int(video['season']),
                     index = int(video['episode'])
+                    )
                 )
-            )
-        
-    if len(oc) < 1:
+        sortOnAirData(episodeOc)
+        for ep in episodeOc.objects:
+            oc.add(ep)
+    elif id == 'video_program' and videosInfo['video_clip'] == None:
         oc.header  = "Sorry"
         oc.message = "No programs found."
-     
+
+    return oc
+
+####################################################################################################
+@route(PREFIX + '/Clips')
+def Clips(base_url, videos_url, title, id, art=None):
+
+    oc = ObjectContainer(title2 = unicode(title))
+
+    videosInfo = JSON.ObjectFromURL(videos_url)
+
+    if id:
+        videos = videosInfo[id]
+    else:
+        videos = videosInfo
+    
+    for clip in videos:
+        
+        oc.add(
+            VideoClipObject(
+                url = base_url + '/play/' + clip['id'],
+                title = unicode(clip['title']),
+                summary = unicode(clip['summary']),
+                thumb = GetImgUrl(clip['image']),
+                art = art,
+                originally_available_at = datetime.date.fromtimestamp(int(clip['created'])),
+                duration = int(clip['length']) * 1000
+                )
+            )
+    sortOnAirData(oc)
     return oc
 
 ####################################################################################################
 def GetImgUrl(url):
     return "http://play.pdl.viaplay.com/imagecache/497x280/" + url.replace('\\', '') 
 
+def sortOnAirData(Objects):
+    for obj in Objects.objects:
+        if obj.originally_available_at == None:
+            Log("JTDEBUG - air date missing for %s" % obj.title)
+            return Objects.objects.reverse()
+    return Objects.objects.sort(key=lambda obj: (obj.originally_available_at,obj.title))
